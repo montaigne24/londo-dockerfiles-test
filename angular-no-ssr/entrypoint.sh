84327be6eaad828f06fd917da6b9432b82d9c874
montaigne23 <76828355+montaigne23@users.noreply.github.com>
2025-06-28 11:58:52 +0100

    Exercice 1 (Rappel sur les Dockerfiles)

--- angular-no-ssr/entrypoint.sh
+++ angular-no-ssr/entrypoint.sh
@@ -4,6 +4,8 @@
 # Vérifie et recrée les répertoires temporaires avec les bonnes permissions
 mkdir -p /var/cache/nginx/client_temp && \
 chown nginx:nginx /var/cache/nginx/client_temp && \
+chown nginx:nginx /var/run/nginx.pid && \
+chmod 755 /var/run/nginx.pid && \
 chmod 755 /var/cache/nginx/client_temp
 
 exec "$@"
